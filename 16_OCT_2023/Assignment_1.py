58833d33cd36db98fa3ad2f5b15a1ae3bfcd7438
Nikhilkathawate <nikhilkathawate82@gmail.com>
2023-10-17 22:11:34 +0530

    Merge pull request #1 from Nikhilkathawate/Nikhilkathawate-patch-1
    Seo HTML Test file drag and drop

---
+++ 16_OCT_2023/Assignment_1.py
@@ -0,0 +1,23 @@
+# Grade Calculator:
+# Write a program that calculates and displays the letter grade for a given numerical score (e.g., A, B, C, D, or F) based on the following grading scale:
+# input- score - 89
+# output- B
+# A: 90-100
+# B: 80-89
+# C: 70-79
+# D: 60-69
+# F: 0-59
+#
+# If, elif, else
+
+score = int(input("Enter your score\n"))
+if 90 <= score <= 100:
+    print("Your grade is - A")
+elif 80 <= score <= 89:
+    print("Your grade is - B")
+elif 70 <= score <= 79:
+    print("Your grade is - C")
+elif 60 <= score <= 69:
+    print("Your grade is - D")
+else:
+    print("Your grade is - F")
